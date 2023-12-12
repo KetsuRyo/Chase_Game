6f1787a9672410aa1ee24bd81a5066ec2be44034
gscqgdw <105544670+gscqgdw@users.noreply.github.com>
2023-12-12 04:12:02 -0500

    Add files via upload

---
+++ Chase_Game-devFinal/src/main/java/com/example/chessgame/GamePiece.java
@@ -0,0 +1,106 @@
+package com.example.chessgame;
+
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
+import javafx.scene.text.Text;
+import org.w3c.dom.events.MouseEvent;
+
+import java.io.InputStream;
+
+
+public class GamePiece {
+    private final String name;     // 棋子的名称
+    private final int strength;    // 棋子的力量等级
+    private int posX;        // 棋子在棋盘上的X坐标
+    private int posY;        // 棋子在棋盘上的Y坐标
+    private final Text pieceName = new Text(""); // 棋子的图形表示
+    private ImageView imageView;
+
+
+
+    public GamePiece(String name, int strength, int posX, int posY,  String  imagePath , GameManager gameManager) {
+        this.name = name;
+        this.strength = strength;
+        this.posX = posX;
+        this.posY = posY;
+        this.pieceName.setText(name);
+        try {
+            InputStream is = getClass().getResourceAsStream(imagePath);
+            if (is == null) {
+                System.out.println("gpp");
+
+            }
+            Image image = new Image(is);  // 正確的構造器用法
+            this.imageView = new ImageView(image);
+            imageView.setFitWidth(50);
+            imageView.setFitHeight(50);
+
+            // 保持图像的纵横比
+            imageView.setPreserveRatio(true);
+        } catch (IllegalArgumentException e) {
+            System.out.println("gtp");
+            // 處理錯誤，例如設置一個預設圖像
+        }
+
+    }
+
+
+
+    // Getter和Setter方法
+    public String getName() {
+        return name;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+
+
+
+
+
+
+
+    // 检查棋子是否可以吃掉另一个棋子
+    public boolean canDefeat(GamePiece other) {
+        // 特殊规则：鼠可以吃象，象不能吃鼠
+        if (this.name.equals("鼠") && other.getName().equals("象")) {
+            return true;
+        }
+        if (this.name.equals("象") && other.getName().equals("鼠")) {
+            return false;
+        }
+
+        // 一般规则：力量等级高的棋子可以吃掉力量等级低的
+        return this.strength >= other.getStrength();
+    }
+
+
+
+    public Node getImageView() {
+        return imageView;
+    }
+
+
+}
---
+++ Chase_Game-devFinal/src/main/java/com/example/chessgame/StartDialog.java
@@ -0,0 +1,78 @@
+package com.example.chessgame;
+
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class StartDialog {
+    private static GameManager gameManager;
+
+    public static void display(Stage primaryStage, PlayerNamesCallback callback) {
+        Stage dialog = new Stage();
+        dialog.initModality(Modality.APPLICATION_MODAL); // 设置为模态窗口
+        dialog.setTitle("玩家信息");
+
+        // 创建文本框让玩家输入姓名
+        TextField playerName1 = new TextField();
+        playerName1.setPromptText("玩家1姓名");
+
+        TextField playerName2 = new TextField();
+        playerName2.setPromptText("玩家2姓名");
+
+        // 创建按钮，当点击时开始游戏
+        Button startButton = new Button("开始游戏");
+        startButton.setOnAction(e -> {
+            // 获取玩家输入的姓名
+            String name1 = playerName1.getText();
+            String name2 = playerName2.getText();
+            Label player1 = new Label("Player1: " + name1);
+            Label player2 = new Label("Player2: " + name2);
+            player2.setFont(Font.font(30));
+            player1.setFont(Font.font(30));
+
+            // 调用回调方法
+            callback.onNamesEntered(name1, name2);
+
+            gameManager = new GameManager(name1, name2);
+            gameManager.startNewGame();
+
+            // 创建游戏界面的根布局
+            BorderPane root = new BorderPane();
+
+            // 将游戏棋盘添加到布局中
+            root.setTop(player1);
+            root.setCenter(gameManager.getGameBoard().getGridPane());
+            root.setBottom(player2);
+
+            // 创建场景，设置尺寸并添加布局
+            Scene scene = new Scene(root, 800, 600);
+
+            // 设置舞台标题
+            primaryStage.setTitle("斗兽棋");
+
+            // 将场景设置到舞台上
+            primaryStage.setScene(scene);
+
+            // 显示舞台
+            primaryStage.show();
+
+            // 关闭对话框
+            dialog.close();
+        });
+
+        VBox layout = new VBox(10);
+        layout.setPadding(new Insets(20, 20, 20, 20));
+        layout.getChildren().addAll(playerName1, playerName2, startButton);
+
+        Scene scene = new Scene(layout, 300, 200);
+        dialog.setScene(scene);
+        dialog.showAndWait();
+    }
+}
